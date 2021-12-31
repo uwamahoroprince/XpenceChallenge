60fdaaecdcf83063d3533018740c77d55d800bd1
uwamahoroprince <thierryprince84@gmail.com>
2021-12-31 16:23:09 +0200

    some config2

--- src/main/java/com/example/expenceApp/ExpenceAppApplication.java
+++ src/main/java/com/example/expenceApp/ExpenceAppApplication.java
@@ -20,7 +20,7 @@ public class ExpenceAppApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000/");
+				registry.addMapping("/**").allowedOrigins("https://expence-front-end.herokuapp.com/");
 			}
 		};
 	}
